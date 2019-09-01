fcaca90f8f841936c8049f55a5ecb9bd4dd2d6ad
maxfolder <maxfolder55@gmail.com>
2019-09-01 10:11:27 +0300

    Клиент сервер, бесконечно обменивающиеся сообщениями

---
+++ client.php
@@ -0,0 +1,35 @@
+<?php
+header('Content-Type: text/plain;'); //Мы будем выводить простой текст
+set_time_limit(0); //Скрипт должен работать постоянно
+ob_implicit_flush(); //Все echo должны сразу же выводиться
+$address = 'localhost'; //Адрес работы сервера
+$port = 8082; //Порт работы сервера (лучше какой-нибудь редкоиспользуемый)
+if (($socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) < 0) {
+    //AF_INET - семейство протоколов
+    //SOCK_STREAM - тип сокета
+    //SOL_TCP - протокол
+    echo "Ошибка создания сокета";
+} else {
+    echo "Сокет создан\n";
+}
+$result = socket_connect($socket, $address, $port);
+if ($result === false) {
+    echo "Ошибка при подключении к сокету";
+} else {
+    echo "Подключение к сокету прошло успешно\n";
+}
+
+do {
+    sleep(1);
+    $out = socket_read($socket, 1024); //Читаем сообщение от сервера
+    echo "Сообщение от сервера: $out.\n";
+    $msg = 'Accepted';
+    socket_write($socket, $msg, strlen($msg)); //Отправляем серверу сообщение
+} while(true);
+
+
+if (isset($socket)) {
+    socket_close($socket);
+    echo "Сокет успешно закрыт";
+}
+
---
+++ server.php
@@ -0,0 +1,64 @@
+<?php
+header('Content-Type: text/plain;'); //Мы будем выводить простой текст
+set_time_limit(0); //Скрипт должен работать постоянно
+ob_implicit_flush(); //Все echo должны сразу же отправляться клиенту
+$address = 'localhost'; //Адрес работы сервера
+$port = 8082; //Порт работы сервера (лучше какой-нибудь редкоиспользуемый)
+if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) < 0) {
+    //AF_INET - семейство протоколов
+    //SOCK_STREAM - тип сокета
+    //SOL_TCP - протокол
+    echo "Ошибка создания сокета";
+} else {
+    echo "Сокет создан\n";
+}
+//Связываем дескриптор сокета с указанным адресом и портом
+if (($ret = socket_bind($sock, $address, $port)) < 0) {
+    echo "Ошибка связи сокета с адресом и портом";
+} else {
+    echo "Сокет успешно связан с адресом и портом\n";
+}
+//Начинаем прослушивание сокета (максимум 5 одновременных соединений)
+if (($ret = socket_listen($sock, 5)) < 0) {
+    echo "Ошибка при попытке прослушивания сокета";
+} else {
+    echo "Ждём подключение клиента\n";
+}
+do {
+    //Принимаем соединение с сокетом
+    if (($msgsock = socket_accept($sock)) < 0) {
+        echo "Ошибка при старте соединений с сокетом";
+    } else {
+        echo "Сокет готов к приёму сообщений\n";
+    }
+    sleep(2);
+    $msg = 'Hi'; //Сообщение клиенту
+    socket_write($msgsock, $msg, strlen($msg)); //Запись в сокет
+    //Бесконечный цикл ожидания клиентов
+    do {
+        if (false === ($buf = socket_read($msgsock, 1024))) {
+            echo "Ошибка при чтении сообщения от клиента";
+        }
+
+        //Если клиент передал exit, то отключаем соединение
+        if ($buf === 'exit') {
+            socket_close($msgsock);
+            break 2;
+        }
+        if (!is_string($buf)) {
+            echo "Сообщение от сервера: передана не строка\n";
+        }
+
+        if ($buf === 'Accepted') {
+            echo sprintf("Сообщение %s принято клиентом\n", $msg);
+        }
+        $msg = mt_rand(1, 10000);
+        socket_write($msgsock, $msg, strlen($msg));
+
+    } while (true);
+} while (true);
+//Останавливаем работу с сокетом
+if (isset($sock)) {
+    socket_close($sock);
+    echo "Сокет успешно закрыт";
+}
